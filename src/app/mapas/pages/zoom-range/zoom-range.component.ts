46db4bc2f9603a497a5fe70b3585231dc3cfc82f
Juan Belda Molina <juan.belda@alicante.es>
2021-01-27 21:04:41 +0100

    Tema 307. Página Zoom-Range - Diseño y tarea

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+    .mapa-container {
+      height: 100%;
+      width: 100%;
+    }
+
+    .row {
+      background-color: white;
+      border-radius: 5px;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      position: fixed;
+      z-index: 999
+    }
+    `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +28,12 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var map = new mapboxgl.Map({
+      container: 'mapa2',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ -0.7484136607062297, 38.25670354590649 ],
+      zoom: 17
+    });
   }
 
 }
