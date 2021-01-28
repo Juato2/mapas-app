5b684cde02c8ad8aa0bce0c24fe55ccea87bfe6d
Juan Belda Molina <juan.belda@alicante.es>
2021-01-28 11:47:33 +0100

    Tema 313. Marcadores en el mapa

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+      .mapa-container {
+        height: 100%;
+        width: 100%;
+      }
+    `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+  // El símbolo "!" le dice a TypeScript que confíe en nosotros
+  // que haremos las validaciones respectivas.
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-0.7484136607062297, 38.25670354590649];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    // const markerHtml: HTMLElement = document.createElement('div');
+    // markerHtml.innerHTML = 'Hola Mundo';
+
+    new mapboxgl.Marker(
+    //   {
+    //   element: markerHtml
+    // }
+    )
+      .setLngLat(this.center)
+      .addTo(this.mapa);
   }
 
 }
