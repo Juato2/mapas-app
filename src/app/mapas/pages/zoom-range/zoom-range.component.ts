050f262591c33ffd1dbc81ee6e18f69611c66dce
Juan Belda Molina <juan.belda@alicante.es>
2021-01-28 11:30:28 +0100

    Tema 312. Obtener las coordenadas centrales del mapa (NOTA: no olvidar destruir los listener en el OnDestroy)

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -6,52 +6,66 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './zoom-range.component.html',
   styles: [
     `
-    .mapa-container {
-      height: 100%;
-      width: 100%;
-    }
+      .mapa-container {
+        height: 100%;
+        width: 100%;
+      }
 
-    .row {
-      background-color: white;
-      border-radius: 5px;
-      bottom: 50px;
-      left: 50px;
-      padding: 10px;
-      position: fixed;
-      width: 400px;
-      z-index: 999
-    }
-    `
-  ]
+      .row {
+        background-color: white;
+        border-radius: 5px;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        position: fixed;
+        width: 400px;
+        z-index: 999;
+      }
+    `,
+  ],
 })
-export class ZoomRangeComponent implements AfterViewInit {
-
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
   // El símbolo "!" le dice a TypeScript que confíe en nosotros
   // que haremos las validaciones respectivas.
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 10;
+  center: [number, number] = [-0.7484136607062297, 38.25670354590649];
 
-  constructor() { }
+  constructor() {}
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
+  }
 
   ngAfterViewInit(): void {
-
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [ -0.7484136607062297, 38.25670354590649 ],
-      zoom: this.zoomLevel
+      center: this.center,
+      zoom: this.zoomLevel,
     });
 
-    this.mapa.on('zoom', () => this.zoomLevel = this.mapa.getZoom())
-    this.mapa.on('zoomend', () =>
-      {
-        if(this.mapa.getZoom() > 18) {
-          // this.mapa.setZoom(18);
-          this.mapa.zoomTo(18);
-        }
+    this.mapa.on('zoom', () => (this.zoomLevel = this.mapa.getZoom()));
+
+    this.mapa.on('zoomend', () => {
+      if (this.mapa.getZoom() > 18) {
+        // this.mapa.setZoom(18);
+        this.mapa.zoomTo(18);
       }
-    )
+    });
+
+    // this.mapa.on('move', () => {
+    //   const { lng, lat } = this.mapa.getCenter();
+    //   this.center = [lng, lat];
+    // });
+    this.mapa.on('move', (event) => {
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [lng, lat];
+    });
   }
 
   zoomOut() {
@@ -62,8 +76,7 @@ export class ZoomRangeComponent implements AfterViewInit {
     this.mapa.zoomIn();
   }
 
-  zoomCambio(valor: string):void {
+  zoomCambio(valor: string): void {
     this.mapa.zoomTo(Number(valor));
   }
-
 }
