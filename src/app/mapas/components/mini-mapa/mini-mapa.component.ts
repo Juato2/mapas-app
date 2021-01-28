589fe04161b224dd825789ac35c4e31a0e7fae6b
Juan Belda Molina <juan.belda@alicante.es>
2021-01-28 19:09:44 +0100

    Tema 321. Componente MiniMapa

--- src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styles: [
+    `
+    div {
+      margin: 0px;
+      height: 150px;
+      width: 100%;
+    }
+    `
   ]
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
+
+  @Input() lngLat: [number, number] = [0,0];
+  @ViewChild('mapa') divMapa!: ElementRef;
 
   constructor() { }
+  ngAfterViewInit(): void {
+    const mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
 
-  ngOnInit(): void {
+    new mapboxgl.Marker()
+      .setLngLat(this.lngLat)
+      .addTo(mapa);
   }
 
 }
